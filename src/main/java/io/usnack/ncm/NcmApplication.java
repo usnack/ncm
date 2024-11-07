1e2b832cae4a503fb99efa4abd2f566989dc737e
usnack <usnack522@gmail.com>
2024-11-07 21:52:56 +0900

    Initial Commit

---
+++ src/main/java/io/usnack/ncm/NcmApplication.java
@@ -0,0 +1,13 @@
+package io.usnack.ncm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/usnack/ncm/NcmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.usnack.ncm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
